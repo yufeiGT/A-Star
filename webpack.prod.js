84352e3e4832cb681e5817a1e6dc6772d115c184
yufeiGT <769416198@qq.com>
2021-06-21 11:53:15 +0800

    save

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,7 +15,7 @@ module.exports = merge(common, {
         libraryTarget: 'umd',
     },
     externals: {
-        '@~crazy/merge': '@~crazy/merge',
-        // 'element-resize-detector': 'elementResizeDetectorMaker',
+        // '@~crazy/merge': '@~crazy/merge',
+        'element-resize-detector': 'elementResizeDetectorMaker',
     },
 });
